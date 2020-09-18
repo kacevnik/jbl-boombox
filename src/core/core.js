f9a804cc917c08ec2d5d170b5c4c1776cf3121db
Kovalev Dmitriy <kacevnik@yandex.ru>
2020-09-18 10:18:08 +0300

    webpack init

---
+++ src/core/core.js
@@ -0,0 +1,4 @@
+import '../components/owlcarousel/owl.carousel.js';
+import '../components/fancybox/fancybox.js';
+import '../components/header/header.js';
+import '../components/events/events-list.js';
---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+
+    entry: {
+        app: [
+            './main.js',
+        ],
+    },
+
+    output: {
+        filename: 'js/script.js',
+        path: path.resolve(__dirname, 'public'),
+        publicPath: '../'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: '/node_modules/'
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: { sourceMap: true}
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: { sourceMap: true }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: true }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: { sourceMap: true}
+                    },
+                ]
+            },
+            {
+                test: /\.(png|jpg|svg|ttf|eot|woff|woff2|otf)$/,
+                loader: 'file-loader',
+                options: {
+                  name: '[path][name].[ext]'
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/style.css"
+        })
+    ]
+
+}
